0bb2a9c6e52519abf45335cb8f8db12fa1846040
DvidVzqz <davisss123x3@gmail.com>
2026-05-01 13:22:36 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tienda.app',
+  appName: 'tiendaVasquez',
+  webDir: 'dist'
+};
+
+export default config;

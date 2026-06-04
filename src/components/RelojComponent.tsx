b5d093924140c1a8b8eb876da1244f474e4ba095
lvasquezDP <luisemmanuel.dp@gmail.com>
2026-06-03 22:09:24 -0600

    fix build

--- src/components/RelojComponent.tsx
+++ src/components/RelojComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Reloj() {
     // 1. Estado inicial con la hora del sistema
